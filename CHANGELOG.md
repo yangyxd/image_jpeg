1052affdd401dd53490fda7de8f712181a385cc5
yangyxd <yangyxd@126.com>
2018-08-31 16:07:23 +0800

    修复bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## [0.0.2] - TODO: Add release date.
+
+
 ## [0.0.1] - TODO: Add release date.
 
 * TODO: Describe initial release.
--- android/src/main/java/com/yangyxd/imagejpeg/ImageJpegPlugin.java
+++ android/src/main/java/com/yangyxd/imagejpeg/ImageJpegPlugin.java
@@ -83,13 +83,29 @@ public class ImageJpegPlugin implements MethodCallHandler {
   }
 
   public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+//    final int height = options.outHeight;
+//    final int width = options.outWidth;
+//    int inSampleSize = 1;
+//    if (height > reqHeight || width > reqWidth) {
+//      final int heightRatio = Math.round((float) height / (float) reqHeight);
+//      final int widthRatio = Math.round((float) width / (float) reqWidth);
+//      inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+//    }
+
     final int height = options.outHeight;
     final int width = options.outWidth;
     int inSampleSize = 1;
+
     if (height > reqHeight || width > reqWidth) {
-      final int heightRatio = Math.round((float) height / (float) reqHeight);
-      final int widthRatio = Math.round((float) width / (float) reqWidth);
-      inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+
+      final int halfHeight = height / 2;
+      final int halfWidth = width / 2;
+
+      // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+      // height and width larger than the requested height and width.
+      while ((halfHeight / inSampleSize) > reqHeight  && (halfWidth / inSampleSize) > reqWidth) {
+        inSampleSize *= 2;
+      }
     }
     return inSampleSize;
   }
